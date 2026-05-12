6141b4e320bfa328f5b0ff59f379ad0a8a980cce
ArthurAmbrozewicz <arthurambrozewicz@gmail.com>
2026-05-12 10:28:07 -0300

    Commit Inicial

---
+++ src/main/java/com/smilehub/smilehub/SmilehubApplication.java
@@ -0,0 +1,14 @@
+package com.smilehub.smilehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmilehubApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/smilehub/smilehub/SmilehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilehub.smilehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmilehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
