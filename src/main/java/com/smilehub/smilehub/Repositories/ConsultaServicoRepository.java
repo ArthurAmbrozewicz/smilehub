0f80d5733d92d389477ad05ce63d3a996f31879c
ArthurAmbrozewicz <arthurambrozewicz@gmail.com>
2026-06-17 21:40:44 -0300

    Fix - Hibernate estava realizando insert antes de delete causando erro ao editar

--- src/main/java/com/smilehub/smilehub/Repositories/ConsultaServicoRepository.java
+++ src/main/java/com/smilehub/smilehub/Repositories/ConsultaServicoRepository.java
@@ -4,6 +4,8 @@ import com.smilehub.smilehub.entities.Consulta;
 import com.smilehub.smilehub.entities.ConsultaServico;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ConsultaServicoRepository extends JpaRepository<ConsultaServico, Long> {
 
@@ -13,5 +15,7 @@ public interface ConsultaServicoRepository extends JpaRepository<ConsultaServico
 
     boolean existsByConsultaIdAndServicoId(Long consultaId, Long servicoId);
 
+    @Modifying
+    @Query("DELETE FROM ConsultaServico cs WHERE cs.consulta.id = :consultaId")
     void deleteByConsultaId(Long consultaId);
 }
--- src/main/java/com/smilehub/smilehub/Repositories/ServicoMaterialRepository.java
+++ src/main/java/com/smilehub/smilehub/Repositories/ServicoMaterialRepository.java
@@ -3,10 +3,14 @@ package com.smilehub.smilehub.repositories;
 import com.smilehub.smilehub.entities.ServicoMaterial;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ServicoMaterialRepository extends JpaRepository<ServicoMaterial, Long> {
 
     List<ServicoMaterial> findAllByServicoId(Long servicoId);
 
+    @Modifying
+    @Query("DELETE FROM ServicoMaterial sm WHERE sm.servico.id = :servicoId")
     void deleteByServicoId(Long servicoId);
 }
